4e505e2ea51af1b21fa680553341c5f3c7ca1675
Unknown <codredd@outlook.com>
2018-04-20 03:23:34 -0700

    Project Creation
    Basic controllers, model classes, business layer, and interfaces created

---
+++ TheBeerCatalogue/TheBeerCatalogue/Controllers/BeerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheBeerCatalogue.Business;
+
+namespace TheBeerCatalogue.Controllers
+{
+    public class BeerController : Controller
+    {
+        private readonly BeerInterface _beerInterface;
+        public BeerController(BeerInterface beerInterface)
+        {
+            _beerInterface = beerInterface;
+        }
+
+        public ActionResult GetBeer()
+        {
+            return View();
+        }
+        // GET: Beer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TheBeerCatalogue/TheBeerCatalogue/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheBeerCatalogue.MvcApplication" Language="C#" %>
