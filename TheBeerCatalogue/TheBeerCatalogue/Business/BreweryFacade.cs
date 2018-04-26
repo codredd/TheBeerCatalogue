c7ad2a76f65896d220d5d7967016c9c33a592800
codredd <codredd@outlook.com>
2018-04-26 01:29:21 -0700

    Update BreweryFacade.cs

--- TheBeerCatalogue/TheBeerCatalogue/Business/BreweryFacade.cs
+++ TheBeerCatalogue/TheBeerCatalogue/Business/BreweryFacade.cs
@@ -8,7 +8,7 @@ namespace TheBeerCatalogue.Business
     {
         WebClient client = new WebClient();
         BreweryList brewery = null;
-        private string beerUrl = "http://api.brewerydb.com/v2/breweries?key=ee8a1a84bc76fd7d7ae6dd0dc45583e3";
+        private string beerUrl = "http://api.brewerydb.com/v2/breweries?key=<put your Brewery DB API here>";
 
         public BreweryList GetBrewery()
         {
